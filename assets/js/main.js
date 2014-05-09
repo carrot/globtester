9e52e138eaf5ef8cd368bcab4730668feb714f2f
Jeff Escalante <hello@jenius.me>
2014-05-09 11:30:17 -0400

    add 'save' functionality
    To do this, we deflate with zlib, convert to base64, and
    serialize to the url hash. Reverse on the way in.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,11 @@
 var mm = require('minimatch'),
+    zlib = require('zlib'),
+    qs = require('query-string'),
     $ = require('zepto-browserify').$;
+
+//
+// minimatch validation
+//
 
 validate();
 
@@ -16,5 +22,36 @@ function validate() {
     } else {
       $(div).removeClass('match');
     }
+  });
+}
+
+//
+// "save" functionality
+//
+
+var params = qs.parse(location.hash)
+
+if (Object.keys(params).length) {
+
+  zlib.inflate(new Buffer(params.p || params['#p'], 'base64'), function(err, res){
+    $('.prompt').val(String(res));
+  });
+
+  zlib.inflate(new Buffer(params.r || params['#r'], 'base64'), function(err, res){
+    $('.results').html(String(res));
+  });
+}
+
+$('.save').on('click', function(){
+  if (location.hash.length) location.hash = ''
+  deflate_hash($('.prompt').val(), 'p')
+  deflate_hash($('.results').html(), 'r')
+});
+
+function deflate_hash(input, key){
+  zlib.deflate(input, function(err, res){
+    obj = {}; obj[key] = res.toString('base64')
+    location.hash += qs.stringify(obj);
+    location.hash += '&'
   });
 }
